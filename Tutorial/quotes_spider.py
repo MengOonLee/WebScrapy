7a48faf725f8c5a57e1c5ecc953cd3c009d3d68f
MengOonLee <darklemon2000@gmail.com>
2022-07-31 09:44:27 +0800

    Tutorial: xpath

--- Tutorial/quotes_spider.py
+++ Tutorial/quotes_spider.py
@@ -2,7 +2,9 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = ['https://quotes.toscrape.com/tag/humor/']
+    start_urls = [
+        "https://quotes.toscrape.com/tag/humor/"
+    ]
 
     def parse(self, response):
         for quote in response.css('div.quote'):
--- Tutorial/tutorial/tutorial/spiders/quotes_spider.py
+++ Tutorial/tutorial/tutorial/spiders/quotes_spider.py
@@ -3,14 +3,12 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'https://quotes.toscrape.com'
+        "https://quotes.toscrape.com/page/1/",
+        "https://quotes.toscrape.com/page/2/"
     ]
-    
+
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall()
-            }
-        yield from response.follow_all(css='ul.pager li.next a', callback=self.parse)
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
