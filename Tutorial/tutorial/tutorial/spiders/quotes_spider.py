41e81b73964390c23bcdc53a7f09c51ef34b5681
MengOonLee <darklemon2000@gmail.com>
2023-09-11 23:12:35 +0800

    Tutorial: Following links

--- Tutorial/tutorial/tutorial/spiders/quotes_spider.py
+++ Tutorial/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,15 +1,18 @@
 import scrapy
-import pathlib
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/"
+        "https://quotes.toscrape.com/page/1/"
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"./data/quotes-{page}.html"
-        pathlib.Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("span small::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall()
+            }
+
+        for href in response.css("ul.pager a::attr(href)"):
+            yield response.follow(href, callback=self.parse)
