a33cc36ffe589751ad5f5aabef29d725fc265e88
MengOonLee <darklemon2000@gmail.com>
2024-08-25 18:36:48 +0800

    Lotus

--- Groceries/Lotus.py
+++ Groceries/Lotus.py
@@ -1,8 +1,23 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import wait, expected_conditions
+from selenium.common.exceptions import TimeoutException
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy import crawler, loader
+from itemloaders import processors
+import logging
+logging.getLogger().setLevel(logging.INFO)
+
+class LotusItem(scrapy.Item):
+    categories = scrapy.Field()
+    name = scrapy.Field(output_processor = processors.TakeFirst())
+    price = scrapy.Field(output_processor = processors.TakeFirst())
+    info = scrapy.Field()
+
+class LotusLoader(loader.ItemLoader):
+    item = LotusItem()
+    # default_output_processor = processors.TakeFirst()
 
 class LotusSpider(scrapy.Spider):
     name = 'Lotus'
@@ -21,52 +36,13 @@ class LotusSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://www.lotuss.com.my/en/product/aus-mutton-loin-slice-5318-74539337"
+            # "https://www.lotuss.com.my/en/category/meat-poultry/meat/parts-weighted"
+            "https://www.lotuss.com.my/en/category/meat-poultry/meat"
         ]
+        
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_categories)
-
-    def parse_categories(self, response):
-        self.driver.get(response.url)
-
-        elem_present = ""
-        while not elem_present:
-            try:
-                elem_present = self.driver.find_element(By.XPATH,
-                    "//div[@class='product-grid-item']"
-                )
-            except:
-                continue
-
-        selector = scrapy.Selector(text=self.driver.page_source)
-        category = selector.css("h1#category-title::text").get()
-        print(category)
-        category_links = selector.css("div.carousel a")
-        if len(category_links)!=0:
-            yield from response.follow_all(category_links,
-                callback=self.parse_categories)
-        else:
-            last_height = self.driver.execute_script(
-                "return document.body.scrollHeight"
-            )
-
-            while True:
-                self.driver.execute_script(
-                    "window.scrollTo(0, document.body.scrollHeight)"
-                )
-                time.sleep(3)
-                new_height = self.driver.execute_script(
-                    "return document.body.scrollHeight"
-                )
-                if new_height==last_height:
-                    break
-                last_height = new_height
-
-            selector = scrapy.Selector(text=self.driver.page_source)
-            item_links = selector.css("div#product-list a")
-
-            yield from response.follow_all(item_links,
-                callback=self.parse_items)
+            request = scrapy.Request(url=url, callback=self.parse_items)
+            yield request
 
     def parse_items(self, response):
         self.driver.get(response.url)
@@ -74,19 +50,79 @@ class LotusSpider(scrapy.Spider):
         elem_present = ""
         while not elem_present:
             try:
-                elem_present = self.driver.find_element(By.XPATH, """
-                    //img[@id='current-product-image']
-                """)
+                elem_present = self.driver.find_element(By.XPATH,
+                    "//div[@id='product-list']")
             except:
                 continue
 
+        # element_present = ""
+        # while not element_present:
+        #     try:
+        #         element_present = wait.WebDriverWait(self.driver, 10).until(
+        #             expected_conditions.presence_of_element_located(
+        #                 (By.XPATH, "//div[@id='product-list']")))
+        #     except TimeoutException:
+        #         pass
+        
         selector = scrapy.Selector(text=self.driver.page_source)
-        item = selector.css("div.MuiBox-root")
-        name = item.css("h1::text").get()
-        price = item.css("::text")[3:6].getall()
-        price = "".join(price)
-        print(name, price)
+        categories = selector.xpath(
+            "//li[contains(@class, 'MuiBreadcrumbs-li')]//text()"
+        ).getall()
 
-process = CrawlerProcess()
+        categories_url = selector.css("div.carousel a")
+        if len(categories_url)!=0:
+            yield from response.follow_all(categories_url,
+                callback=self.parse_items)
+
+        else:
+            last_height = self.driver.execute_script(
+                "return document.body.scrollHeight")
+            while True:
+                self.driver.execute_script(
+                    "window.scrollTo(0, document.body.scrollHeight)")
+                time.sleep(3)
+                new_height = self.driver.execute_script(
+                    "return document.body.scrollHeight")
+                if new_height==last_height:
+                    break
+                last_height = new_height
+            
+            items = selector.css("div.product-grid-item")
+            for item in items:
+                loader = LotusLoader()
+
+                loader.add_value("categories", categories)
+                
+                name = item.css("a#product-title::text").get()
+                loader.add_value("name", name)
+
+                price = item.css("p")[0].css("::text").getall()
+                loader.add_value("price", "".join(price))
+
+                yield loader.load_item()
+
+                # img_url = item.css("img::attr(src)").get()
+
+                # info_page = item.css("a::attr(href)").get()
+                # if info_page is not None:
+                #     yield response.follow(info_page,
+                #         callback=self.parse_info)
+
+            # info_links = selector.css("div#product-list a")
+            # yield from response.follow_all(info_links,
+            #     callback=self.parse_info)
+
+    # def parse_info(self, response):
+    #     self.driver.get(response.url)
+
+    #     selector = scrapy.Selector(text=self.driver.page_source)
+    #     info = selector.css("div.MuiBox-root::text")[2].get()
+    #     print(info)
+    #     yield info
+
+
+process = crawler.CrawlerProcess(
+    # settings={"FEEDS":{"items.jl":{"format":"jsonlines"}}}
+)
 process.crawl(LotusSpider)
 process.start()
